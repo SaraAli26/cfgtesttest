72d857f958e3a0a27dc8ee7666aa377c390d7b43
Sara Ahmed <saraahmed2693@gmail.com>
2024-09-23 15:11:02 +0300

    Working end point

---
+++ src/main/java/com/example/demo/controllers/PingController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PingController {
+
+    @RequestMapping(path = "/ping", method = RequestMethod.GET)
+    @ResponseBody
+    public String ping(){
+        return "Ping yoooou";
+    }
+}
